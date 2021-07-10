233128c45ed13389f4637b5e681a3bc3a2e45a2a
Zabauk <contact@zabauk.com>
2021-07-10 22:40:00 +0630

    auth added to get posts

--- routes/PostRouter.js
+++ routes/PostRouter.js
@@ -2,7 +2,7 @@ const router=require('express').Router()
 const PostController=require('../controllers/PostController')
 const Auth=require('../middlewares/auth')
 //get all posts
-router.get('/posts', PostController.posts)
+router.get('/posts', Auth, PostController.posts)
 
 //create new post
 router.post('/create-post', Auth, PostController.create)
